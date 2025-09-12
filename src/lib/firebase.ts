cfeb5437de16786ed8a0606413c2e18e49c91e4a
Amol Waghmare <amol.waghmare0521@gmail.com>
2025-09-12 13:06:21 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {"apiKey":"your-api-key","authDomain":"your-auth-domain","projectId":"your-project-id","storageBucket":"your-storage-bucket","messagingSenderId":"your-messaging-sender-id","appId":"your-app-id","measurementId":"your-measurement-id"};
+const firebaseConfig = {"projectId":"studio-2982935128-8a237","appId":"1:1069453338660:web:36a3d17819c078d97150b1","storageBucket":"studio-2982935128-8a237.firebasestorage.app","apiKey":"AIzaSyCpPxyWw1tB0q1vfLqu4ghM6b-rbE_PLQY","authDomain":"studio-2982935128-8a237.firebaseapp.com","measurementId":"","messagingSenderId":"1069453338660"};
 
 // Initialize Firebase
 let app;
