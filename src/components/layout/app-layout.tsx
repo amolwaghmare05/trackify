ac0c9018e3a0783a7c725276a504883264bf52bf
Amol Waghmare <amol.waghmare0521@gmail.com>
2025-09-15 13:34:32 +0000

    not starting.

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,48 +1,30 @@
 
 'use client';
 
-import { Sidebar, SidebarProvider, useSidebar } from '@/components/ui/sidebar';
+import { Sidebar, SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarNav } from '@/components/layout/sidebar-nav';
 import { Header } from '@/components/layout/header';
 import { cn } from '@/lib/utils';
 import * as React from 'react';
 
-function MainContent({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      className={cn(
-        'flex-1 flex flex-col transition-all duration-300 ease-in-out',
-        'md:pl-[var(--sidebar-width-collapsed)] group-data-[sidebar-open=true]/sidebar-wrapper:md:pl-[var(--sidebar-width)]'
-      )}
-    >
-      <Header />
-      <main className="flex-1 bg-background text-foreground">
-        {children}
-      </main>
-    </div>
-  );
-}
-
 export function AppLayout({ children }: { children: React.ReactNode }) {
-    const [mounted, setMounted] = React.useState(false);
-
-    React.useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return null;
-    }
-
     return (
         <SidebarProvider>
             <div className="relative flex min-h-screen">
                 <Sidebar>
-                <SidebarNav />
+                    <SidebarNav />
                 </Sidebar>
-                <MainContent>
-                {children}
-                </MainContent>
+                <div
+                    className={cn(
+                        'flex-1 flex flex-col transition-all duration-300 ease-in-out',
+                        'md:pl-[var(--sidebar-width-collapsed)] group-data-[sidebar-open=true]/sidebar-wrapper:md:pl-[var(--sidebar-width)]'
+                    )}
+                >
+                    <Header />
+                    <main className="flex-1 bg-background text-foreground">
+                        {children}
+                    </main>
+                </div>
             </div>
         </SidebarProvider>
     );
